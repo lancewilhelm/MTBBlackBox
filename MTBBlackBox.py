82375a1a43d2244db1ba0440e5416cb89177c904
Lance <lance.wilhelm@gmail.com>
2019-05-07 23:02:37 -0400

    changed bunch o shit fo dmp trials. fingers crossed

--- MTBBlackBox.py
+++ MTBBlackBox.py
@@ -89,9 +89,9 @@ def main(args):
                 print('roll: ' + str(roll_pitch_yaw.x))
                 print('pitch: ' + str(roll_pitch_yaw.y))
                 print('yaw: ' + str(roll_pitch_yaw.z))
-                print('accX: ' str(accel.x))
-                print('accY: ' str(accel.y))
-                print('accZ: ' str(accel.z))
+                print('accX: ' + str(accel.x))
+                print('accY: ' + str(accel.y))
+                print('accZ: ' + str(accel.z))
 
                 full_row_temp = [("%.3f" % t), ("%.4f" % roll_pitch_yaw.x), ("%.4f" % roll_pitch_yaw.y), ("%.4f" % roll_pitch_yaw.z), ("%.4f" % accel.x), ("%.4f" % accel.y), ("%.4f" % accel.z)]
 
