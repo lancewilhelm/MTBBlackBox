b9b8f6db13895e5693dccbf7b2e02123ec926d9f
Lance <lance.wilhelm@gmail.com>
2019-05-10 20:34:05 -0600

    fixed blink.cpp, added one more LED

--- blink.cpp
+++ blink.cpp
@@ -1,14 +1,17 @@
-#include <wiringPi>
+#include <wiringPi.h>
 
 int main() {
 
   wiringPiSetup();
-  pinmode(0, OUTPUT);
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT)
 
   for(;;){
     digitalWrite(0, HIGH);
+    digitalWrite(1, LOW);
     delay(500);
     digitalWrite(0, LOW);
+    digitalWrite(1, HIGH);
     delay(500);
   }
 
