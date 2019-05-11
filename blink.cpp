c262ec4ea1b3a31f4eb9dedae71a50b8c81410d7
Lance <lance.wilhelm@gmail.com>
2019-05-10 20:05:11 -0600

    testing wiringPi with blink file

---
+++ blink.cpp
@@ -0,0 +1,16 @@
+#include <wiringPi>
+
+int main() {
+
+  wiringPiSetup();
+  pinmode(0, OUTPUT);
+
+  for(;;){
+    digitalWrite(0, HIGH);
+    delay(500);
+    digitalWrite(0, LOW);
+    delay(500);
+  }
+
+  return 0;
+}
