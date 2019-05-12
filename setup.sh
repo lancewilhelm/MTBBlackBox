4a0c5d1bc7ce38c6ff5dc8181fd6188fe6899f8e
Lance <lance.wilhelm@gmail.com>
2019-05-12 12:25:56 -0600

    changes to setup file. needs more

--- setup.sh
+++ setup.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 sudo apt-get install -y i2c-tools
 
+# Needed for LEDs and Buttons
 cd /tmp
 wget https://lion.drogon.net/wiringpi-2.50-1.deb
 sudo dpkg -i wiringpi-2.50-1.deb
 
+# For I2C stuff
 cd ~/mtbblackbox
 sudo cp ~/mtbblackbox/setup/modules /etc/
 sudo cp ~/mtbblackbox/setup/config.txt /boot/
 
+# For run from reboot
 sudo rm /var/spool/cron/crontabs/pi
 sudo cp ~/mtbblackbox/setup/crontabScript /var/spool/cron/crontabs/pi
 sudo chmod 600 /var/spool/cron/crontabs/pi
+
+# For GPS
+sudo systemctl disable serial-getty@ttyAMA0.service
 
 sudo reboot now
