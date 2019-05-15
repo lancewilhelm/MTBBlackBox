7abbd53c9aec53bb055677dfd7c27f1eb33d0ec9
Lance <lance.wilhelm@gmail.com>
2019-05-14 20:53:13 -0600

    messing with gps_rec.waiting time. shortened to 1000 micro seconds.

--- mtbbb.cpp
+++ mtbbb.cpp
@@ -221,15 +221,15 @@ void loop(std::ofstream &myfile, std::chrono::high_resolution_clock::time_point 
         // FIX THIS
         // Get GPS goodies if setup did not fail and we are not waiting for a packet
         if(!gpsfail && gps_rec.waiting(1000)){
-          // std::cout << "GPS READY" << std::endl;
+          std::cout << "GPS READY" << std::endl;
         //
           // create a structure for the data
           // struct gps_data_t *gpsd_data;
         //
-          // if ((gpsd_data = gps_rec.read()) == NULL) {
-          //   std::cerr << "GPSD read error.\n";
-          //   gpsfail = true;
-          // } else {
+          if ((gpsd_data = gps_rec.read()) == NULL) {
+            std::cerr << "GPSD read error.\n";
+            gpsfail = true;
+          } else {
         //     if (((gpsd_data = gps_rec.read()) == NULL) ||
         //            (gpsd_data->fix.mode < MODE_2D)) {
         //       return;
@@ -254,7 +254,7 @@ void loop(std::ofstream &myfile, std::chrono::high_resolution_clock::time_point 
         //     std::cout.setf(std::ios::fixed, std::ios::floatfield);
         //     std::cout << "gpsTime: " << time_str << ", Lat: " << latitude << ",  Lon: " << longitude << ", Sp: " << speed << ", Alt: " << alt << std::endl;
         //     std::cout << time_str << "," << latitude << "," << longitude << "," << speed << "," << alt << std::endl;
-          // }
+          }
         }
     }
 }
