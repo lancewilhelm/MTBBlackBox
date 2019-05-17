1466ec4c58e49c0d08b862530230ddda5b9cb189
Lance <lance.wilhelm@gmail.com>
2019-05-17 17:33:13 -0600

    added oled things bit by bit...

--- mtbbb.cpp
+++ mtbbb.cpp
@@ -236,7 +236,7 @@ void loop(std::ofstream &myfile, std::chrono::high_resolution_clock::time_point 
         auto oled_time_str { osss.str() };
 
         // If we have received new GPS data, update the screen
-        if(newGPSData){
+        if(false){ //fix this
 
           // display current GPS state
           if(seconds == 0){
