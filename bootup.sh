4d7f3aa481ac926864e02456fbabbc2cadfbbdc0
Lance <lance.wilhelm@gmail.com>
2019-05-18 00:00:01 -0600

    modifying boot code for init.d. ignoring text files in repo

--- bootup.sh
+++ bootup.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
-echo "ACTIVATE BLACKICE NEBULA STARTRON"
-/home/pi/mtbblackbox/mtbbb
+
+### BEGIN INIT INFO
+# Provides:          mtbbb
+# Required-Start:    $local_fs $network
+# Required-Stop:     $local_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: mtbbb service
+# Description:       Run mtbbb service
+### END INIT INFO
+
+# Carry out specific functions when asked to by the system
+case "$1" in
+  start)
+    echo "Starting mtbbb..."
+    sudo -u pi bash -c 'sudo /home/pi/mtbblackbox/mtbbb &> log.txt'
+    ;;
+  stop)
+    echo "Stopping mtbbb..."
+    sudo -u pi bash -c 'cd /path/to/scripts/ && ./stop-mtbbb.sh'
+    sleep 2
+    ;;
+  *)
+    echo "Usage: /etc/init.d/mtbbb {start|stop}"
+    exit 1
+    ;;
+esac
+
+exit 0
