0931fa5e6f2f126243a0f7cebbcfb170087e4b64
Lance <lance.wilhelm@gmail.com>
2019-05-18 22:45:09 -0600

    Attempting to write logo, removing font sizes

--- mtbbb.cpp
+++ mtbbb.cpp
@@ -84,8 +84,9 @@ void setup() {
 
     // OLED Init
     int i = oledInit(iChannel, iOLEDAddr, iOLEDType, bFlip, bInvert);
-    oledFill(0x00); // Clear the screen
+    // oledFill(0x00); // Clear the screen
     oledWriteLogo();
+    usleep(100000);
     oledWriteString(2,0,"INITIALIZING...");
 
     // Setup the GPIO stuff for the LEDs and buttons
