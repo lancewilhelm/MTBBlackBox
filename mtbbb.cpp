104a6f4a1a8b7d27b2f4b98f8deda1a135c40e30
Lance <lance.wilhelm@gmail.com>
2019-05-25 23:58:15 -0600

    subtracted 1 from accZworld. Subtracted 27.21 from pitch due to setup on bike

--- mtbbb.cpp
+++ mtbbb.cpp
@@ -219,14 +219,14 @@ void loop(std::ofstream &myfile, std::chrono::high_resolution_clock::time_point 
 
         // Yaw Pitch Roll
         // std::cout << std::fixed << std::setprecision(2) << "ypr: " << (ypr[0] * 180/M_PI) << "," << (ypr[1] * 180/M_PI) << "," << (ypr[2] * 180/M_PI) << std::endl;
-        myfile << std::fixed << std::setprecision(2) << (ypr[0] * 180/M_PI) << "," << (ypr[1] * 180/M_PI) << "," << (ypr[2] * 180/M_PI) << ",";
+        myfile << std::fixed << std::setprecision(2) << (ypr[0] * 180/M_PI) << "," << ((ypr[1] * 180/M_PI) - 27.21) << "," << (ypr[2] * 180/M_PI) << ",";
 
         // display real acceleration, adjusted to remove gravity
         // myfile << (static_cast<float>(aaReal.x) / 4096) << "," << (static_cast<float>(aaReal.y) / 4096) << "," << (static_cast<float>(aaReal.z) / 4096) << ",";
 
         // display initial world-frame acceleration, adjusted to remove gravity
         // and rotated based on known orientation from quaternion
-        myfile << (static_cast<float>(aaWorld.x) / 4096) << "," << (static_cast<float>(aaWorld.y) / 4096) << "," << (static_cast<float>(aaWorld.z) / 4096) << ",";
+        myfile << (static_cast<float>(aaWorld.x) / 4096) << "," << (static_cast<float>(aaWorld.y) / 4096) << "," << ((static_cast<float>(aaWorld.z) / 4096) - 1) << ",";
 
         // Display and wriet the GPS data
         timestamp_t ts { gpsd_data->fix.time };
